0d8e39544258e7da111c652a56e6bc1cc2385c98
Sanket <sanket.shirolkar@aimedatsolutions.com>
2022-05-21 18:14:35 +0530

    type Coercion

--- operators.js
+++ operators.js
@@ -1,19 +1,16 @@
-console.log(typeof 4.5);
 
-console.log(typeof "abc");
+//Tyoe Coercion
 
-console.log(typeof function() {});
+console.log(null);
 
-const abc = {a:1, b:"abc"}
-console.log(typeof abc);
-console.log(typeof abc.a);
-console.log(typeof abc.b);
+console.log("5" * 2); 
 
+console.log("five" * 0); 
 
+console.log(5 - "1"); //4
 
-console.log("2" == 2 );
-console.log("2" === 2 );
+console.log(5 + "1"); // 51
 
+console.log(null == undefined); //true
 
-const temp = false;
-console.log(temp ? 1 : 2);
+console.log(null == 0); //false
