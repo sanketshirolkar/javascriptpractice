572c23e46d2e2656b1472059b78db20c3eb38bc6
Sanket <sanket.shirolkar@aimedatsolutions.com>
2022-05-21 17:49:23 +0530

    test

---
+++ operators.js
@@ -0,0 +1,13 @@
+console.log(typeof 4.5);
+
+console.log(typeof "abc");
+
+console.log(typeof function() {});
+
+const abc = {a:1, b:"abc"}
+console.log(typeof abc);
+console.log(typeof abc.a);
+
+
+console.log("2" == 2 );
+console.log("2" === 2 );
